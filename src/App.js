a37d0cc1e6cca9aab5c8182837f5ce909c26dcac
Minhazul Ashim <ashim.ashim7470@gmail.com>
2022-01-02 00:07:12 +0600

    boiler plate is updated

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <h1>Yes! I am Ready...</h1>
-=======
-      <h1>Are You ready Guys???</h1>
-      <h1>Changes made by Ashim</h1>
->>>>>>> 5ae32aae2245a62ec511edb14cbc5ad9b0e3125d
+      <h1>Now the boiler plates are created.</h1>
     </div>
   );
 }
