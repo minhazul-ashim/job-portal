3eaaa19b710caf66df9400fdc8bf596a18eb38d1
ahhridoy <ahhridoyy28@gmail.com>
2022-01-02 15:31:51 +0600

    footer

--- src/Component/Shared/Footer/Footer.js
+++ src/Component/Shared/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const Footer = () => {
     return (
         <div>
-            
+            <Row>
+                <Col xs={12} md={3}>
+                    <h3>Job Portal</h3>
+                </Col>
+            </Row>
         </div>
     );
 };
