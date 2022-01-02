1063fb2786f160f2e649a861f2286aa8f919e8f1
ahhridoy <ahhridoyy28@gmail.com>
2022-01-02 18:35:00 +0600

    contact start

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "YOUR_SERVICE_ID",
+                "YOUR_TEMPLATE_ID",
+                form.current,
+                "YOUR_USER_ID"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
     return (
-        <div>
-            <h1>Please Feel Free to Contact</h1>
-        </div>
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
     );
 };
 
