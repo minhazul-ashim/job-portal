624468373cfa996240cbe8041441004fea8cb1f6
ahhridoy <ahhridoyy28@gmail.com>
2022-01-02 18:12:02 +0600

    footer updated

--- src/Component/Shared/Footer/Footer.js
+++ src/Component/Shared/Footer/Footer.js
@@ -2,45 +2,48 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo from "../../../Images/logo.png";
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div className="bg-dark pt-5 pb-2">
+        <div className="bg-dark pt-5 pb-3">
             <Container>
-                <Row className="mx-auto">
+                <Row className="mx-auto text-start">
                     <Col xs={12} md={3} className="text-light">
                         <Link to="/home">
                             <img className="w-30" src={logo} alt="Main Logo" />
                         </Link>
-                        <p>
+                        <p className="mt-3">
                             We collect reviews from our users go you can get an
                             honest opinion of what an experience with our
                             website.
                         </p>
                     </Col>
-                    <Col xs={12} md={3} className="text-light">
+                    <Col xs={12} md={3} className="link-text">
                         <h5>Quick Links</h5>
-                        <a href="#" className="text-decoration-none">Work</a> <br />
-                        <a href="#" className="text-decoration-none">Services</a> <br />
-                        <a href="#" className="text-decoration-none">Products</a> <br />
-                        <a href="#" className="text-decoration-none">Tips & Tricks</a> <br />
+                        <a href="#">Work</a> <br />
+                        <a href="#">Services</a> <br />
+                        <a href="#">Products</a> <br />
+                        <a href="#">Tips & Tricks</a> <br />
                     </Col>
-                    <Col xs={12} md={3} className="text-light">
+                    <Col xs={12} md={3} className="link-text">
                         <h5>For Employers</h5>
-                        <a href="#" className="text-decoration-none">Air Frieght</a> <br />
-                        <a href="#" className="text-decoration-none">Ocean Freight</a> <br />
-                        <a href="#" className="text-decoration-none">Large Projects</a> <br />
-                        <a href="#" className="text-decoration-none">Job Packages</a> <br />
+                        <a href="#">Air Frieght</a> <br />
+                        <a href="#">Ocean Freight</a> <br />
+                        <a href="#">Large Projects</a> <br />
+                        <a href="#">Job Packages</a> <br />
                     </Col>
-                    <Col xs={12} md={3} className="text-light">
+                    <Col xs={12} md={3} className="link-text">
                         <h5>For Candidates</h5>
-                        <a href="#" className="text-decoration-none">Browse Jobs</a> <br />
-                        <a href="#" className="text-decoration-none">Ocean Freight</a> <br />
-                        <a href="#" className="text-decoration-none">Candidate Dashboard</a> <br />
-                        <a href="#" className="text-decoration-none">Large Projects</a> <br />
-                        <a href="#" className="text-decoration-none">Job Alerts</a> <br />
+                        <a href="#">Browse Jobs</a> <br />
+                        <a href="#">Ocean Freight</a> <br />
+                        <a href="#">Candidate Dashboard</a> <br />
+                        <a href="#">Large Projects</a> <br />
+                        <a href="#">Job Alerts</a> <br />
                     </Col>
                 </Row>
+                <hr className="text-light mt-3" />
+                <small className="text-light">All rights are reserved.</small>
             </Container>
         </div>
     );
