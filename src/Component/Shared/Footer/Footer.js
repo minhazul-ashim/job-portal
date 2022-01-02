cbd3e899a2c1f83c4dacfb7a3aa27213cdc93367
ahhridoy <ahhridoyy28@gmail.com>
2022-01-02 15:47:50 +0600

    footer edited

--- src/Component/Shared/Footer/Footer.js
+++ src/Component/Shared/Footer/Footer.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Col, Row } from 'react-bootstrap';
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import logo from "../../../Images/logo.png";
 
 const Footer = () => {
     return (
         <div>
             <Row>
                 <Col xs={12} md={3}>
-                    <h3>Job Portal</h3>
+                    <Link to="/home">
+                        <img className="w-100" src={logo} alt="Main Logo" />
+                    </Link>
                 </Col>
             </Row>
         </div>
