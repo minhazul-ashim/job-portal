8e0289931f35ff7a55ca1f505c35782ea89b71a0
Minhazul Ashim <ashim.ashim7470@gmail.com>
2022-01-04 00:12:58 +0600

    some overlay added in job post form

--- src/Component/Postjob/Postjob.js
+++ src/Component/Postjob/Postjob.js
@@ -3,83 +3,96 @@ import { Alert, Button, Container } from "react-bootstrap";
 import "./Postjob.css";
 
 const Postjob = () => {
+
     const [success, setSuccess] = useState(false);
+
     return (
-        <Container className="contact-form post-job-img">
-            <form className="w-50">
-                <h2 className="text-primary fw-bold py-2">Post a job</h2>
-                <input
-                    type="text"
-                    placeholder="Job title"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Job type"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Company name"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Location"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Posting date"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Deadline"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <input
-                    type="text"
-                    placeholder="Job"
-                    className="form-control"
-                    required
-                ></input>{" "}
-                <br /> <br />
-                <textarea
-                    name="message"
-                    placeholder="Job description"
-                    className="form-control"
-                    required
-                ></textarea>{" "}
-                <br />
-                <Button
-                    value="send"
-                    className="form-control"
-                    type="submit"
-                    variant="primary"
-                >
-                    Post
-                </Button>{" "}
-                <br /> <br />
-                {success && (
-                    <Alert variant="primary">
-                        Your post Successfully!
-                    </Alert>
-                )}
-            </form>
-        </Container>
+
+        <div className="p-5">
+
+            <Container className="contact-form post-job-img">
+
+                <div className="postJob-overlay">
+
+                    <form className="w-75 mx-auto">
+
+                        <h2 className="text-dark fw-bold my-5">Post Job Requirement Informations</h2>
+
+                        <input
+                            type="text"
+                            placeholder="Job title"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Job type"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Company name"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Location"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Posting date"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Deadline"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <input
+                            type="text"
+                            placeholder="Job"
+                            className="form-control"
+                            required
+                        ></input>{" "}
+                        <br /> <br />
+                        <textarea
+                            name="message"
+                            placeholder="Job description"
+                            className="form-control"
+                            required
+                        ></textarea>{" "}
+                        <br />
+                        <Button
+                            value="send"
+                            className="form-control"
+                            type="submit"
+                            variant="primary"
+                        >
+                            Post
+                        </Button>{" "}
+                        <br /> <br />
+                        {success && (
+                            <Alert variant="primary">
+                                Your post Successfully!
+                            </Alert>
+                        )}
+                    </form>
+                </div>
+
+            </Container>
+        </div>
     );
 };
 
