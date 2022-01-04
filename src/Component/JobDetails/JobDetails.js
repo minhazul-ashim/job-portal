a296949f45cd49e429b485edebcc1368a846caf3
Ashraful <ashrafulislam2880@gmail.com>
2022-01-04 22:44:35 +0600

    Unnessary Code Removed

--- src/Component/JobDetails/JobDetails.js
+++ src/Component/JobDetails/JobDetails.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Col, Container, FloatingLabel, Form, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import swal from 'sweetalert';
 import './Jobdetails.css';
