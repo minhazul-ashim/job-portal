e63d6f57cb4045d576b4cd50d142ace540d30927
ahhridoy <ahhridoyy28@gmail.com>
2022-01-05 15:38:08 +0600

    search option added

--- src/Component/Home/Search/Search.js
+++ src/Component/Home/Search/Search.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { Button, Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
+import React, { useState } from "react";
+import {
+    Button,
+    Col,
+    Container,
+    Row,
+} from "react-bootstrap";
 
 const Search = () => {
+    const [searchTerm, setSearchTerm] = useState("");
+    const searchHandler = (e) => {
+        e.preventDefault();
+        console.log(searchTerm)
+    };
     return (
         <Container className="mt-2">
             <Row>
                 <Col xs={12} md={12} lg={6} className="mx-auto">
-                    <InputGroup className="my-3">
-                        <FormControl placeholder="Search By Job Title" style={{borderRadius:"300px",marginright:"5px", padding:"8px", paddingLeft:"30px"}}/>
-                        <Button style={{borderRadius:"300px",marginLeft:"5px"}}>Search Job</Button>
-                    </InputGroup>
+                    <form className="my-3" onSubmit={searchHandler}>
+                        <input
+                            placeholder="Search By Job Title"
+                            type="text"
+                            value={searchTerm}
+                            onChange={e => setSearchTerm(e.target.value)}
+                            style={{
+                                width: "50%",
+                                borderRadius: "300px",
+                                marginright: "5px",
+                                padding: "8px",
+                                paddingLeft: "30px",
+                            }}
+                        />
+                        <Button
+                            style={{ borderRadius: "300px", marginLeft: "5px" }}
+                            type="submit"
+                        >
+                            Search Job
+                        </Button>
+                    </form>
                 </Col>
             </Row>
         </Container>
